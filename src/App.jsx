c40a885b1ead007a1c8ceb13c2e33e346a954871
HH <shweehow@gmail.com>
2025-08-25 18:00:35 +0800

    Make sidebar collapsable

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
     <div>
-      <Navbar/>
+      <Navbar setSidebar={setSidebar}/>
       <Routes>
         <Route path='/' element={<Home sidebar={sidebar}/>} />
         <Route path='/video/:categoryId/:videoId' element={<Video/>} />
