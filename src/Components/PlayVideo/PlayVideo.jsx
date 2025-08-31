3ebb596568a352d4fbe03c451ce7d65e3b938d22
HH <shweehow@gmail.com>
2025-08-31 18:54:08 +0800

    Adjusted navbar responsive style

---
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './PlayVideo.css'
+import video1 from '../../assets/video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
+
+const PlayVideo = () => {
+  return (
+    <div className='playVideo'>
+      <video src={video1}></video>
+    </div>
+  )
+}
+
+export default PlayVideo
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Video.css'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 
 const Video = () => {
   return (
-    <div>
-      
+    <div className='playCon'>
+      <PlayVideo />
     </div>
   )
 }
